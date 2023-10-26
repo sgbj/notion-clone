7e1d4ca6188e66c3ea06247f0d4c3b436819cabf
sgbj <scott.batary@gmail.com>
2023-10-25 21:30:08 -0500

    added shadcn-ui, theming, layout, and landing page

---
+++ hooks/use-scroll-top.tsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+
+export const useScrollTop = (threshold = 10) => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > threshold);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [threshold]);
+
+  return scrolled;
+};
